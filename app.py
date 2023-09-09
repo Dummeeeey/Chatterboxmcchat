e64380649aa44693b01d500766fffbb5a8b4b72e
Dummeeeey <144566540+Dummeeeey@users.noreply.github.com>
2023-09-09 16:39:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, request, jsonify
 from requests import post
+from sys import stderr
 
 app = Flask("app")
 
 @app.post("/")
 def forward():
-	print("recieved")
 	if request.is_json:
 		data = request.get_json()
+		print(data["webhook"], file=stderr)
+		print(data["data"], file=stderr)
 		post(data["webhook"], data["data"])
 		return data,201
 	return {"error":"yousuckpleaselockin"},415
