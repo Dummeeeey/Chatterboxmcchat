604f708b98fbead8cda897da88e42cdf619c05ef
Dummeeeey <144566540+Dummeeeey@users.noreply.github.com>
2023-09-09 16:01:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
 from requests import post
 
-app = Flask("app")
+app = Flask("app",host="0.0.0.0")
 
 @app.post("/")
 def forward():
+	print("recieved")
 	if request.is_json:
 		data = request.get_json()
 		post(data["webhook"], data["data"])
